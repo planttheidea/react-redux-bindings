101c7bb816cd84a05e5efab6a77911fe2e67d261
Tony Quetano <tony.quetano@planttheidea.com>
2022-08-28 09:41:50 -0400

    add build script (TBD: environment-specific builds)

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
@@ -11,7 +12,7 @@ const EXTERNALS = [
 const EXTENSIONS = ['.js', '.ts', '.tsx'];
 
 const DEFAULT_OUTPUT = {
-  exports: 'default',
+  exports: 'named',
   globals: {
     'hoist-non-react-statics': 'hoistNonReactStatics',
     react: 'React',
@@ -31,6 +32,7 @@ const DEFAULT_CONFIG = {
     { ...DEFAULT_OUTPUT, file: pkg.module, format: 'es' },
   ],
   plugins: [
+    commonjs({ include: /use-sync-external-store/ }),
     resolve({
       extensions: EXTENSIONS,
       mainFields: ['module', 'jsnext:main', 'main'],
